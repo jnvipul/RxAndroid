0993ad07ca3d7e3b64bfd4d841aa0a40819d4298
Vipul J <messagevipul@gmail.com>
2016-09-26 12:24:11 +0800

    Creating 1st Observable and Subscriber

--- app/src/main/java/com/version2/rxandroid/MainActivity.java
+++ app/src/main/java/com/version2/rxandroid/MainActivity.java
@@ -1,13 +1,70 @@
 package com.version2.rxandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.text_view)
+    TextView mTextView;
+
+    Observable.OnSubscribe mObservableAction = new Observable.OnSubscribe<String>() {
+        @Override
+        public void call(final Subscriber<? super String> subscriber) {
+            subscriber.onNext("Version 2.0");
+            subscriber.onCompleted();
+        }
+    };
+
+    Observable<String> mObservable = Observable.create(mObservableAction);
+
+    Subscriber<String> mTextViewSubscriber =  new Subscriber<String>() {
+        @Override
+        public void onCompleted() {}
+
+        @Override
+        public void onError(final Throwable e) {}
+
+        @Override
+        public void onNext(final String s) {
+            mTextView.setText(s);
+        }
+    };
+
+    Subscriber<String> mToastSubscriber = new Subscriber<String>() {
+        @Override
+        public void onCompleted() {}
+
+        @Override
+        public void onError(final Throwable e) {}
+
+        @Override
+        public void onNext(final String s) {
+            Toast.makeText(MainActivity.this, s, Toast.LENGTH_LONG).show();
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setup();
+    }
+
+    private void setup() {
+        mObservable.observeOn(AndroidSchedulers.mainThread());
+        mObservable.subscribe(mTextViewSubscriber);
+        mObservable.subscribe(mToastSubscriber);
     }
 }
